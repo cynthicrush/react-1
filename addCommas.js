67553c50abf0d6dc0a287dd61e94796bcdacfe8a
cynthicrush <cynthiazcanada@gmail.com>
2022-05-17 12:11:18 -0500

    code updated

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,9 @@
-function addCommas() {}
+function addCommas(number) {
+    return number.toLocaleString('en-US')
+}
+
+console.log(addCommas(1234567))
+console.log(addCommas(1234.567))
+console.log(addCommas(-3141592.65))
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,9 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+  test('does it work properly', () => {
+    expect(addCommas(1234567)).toBe('1,234,567')
+    expect(addCommas(12345.67)).toBe('12,345.67')
+    expect(addCommas(-3141592.65)).toBe('-3,141,592.65')
+  })
 });
